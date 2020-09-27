50e8f6327b516daef5363a696ec0b2f257eeef80
acebogo <koprodev@gmail.com>
2020-09-27 23:07:45 +0900

    Update 기능 코드 추가

--- src/main/java/com/bogo/springboot/service/posts/PostsService.java
+++ src/main/java/com/bogo/springboot/service/posts/PostsService.java
@@ -1,8 +1,10 @@
 package com.bogo.springboot.service.posts;
 
+import com.bogo.springboot.domain.posts.Posts;
 import com.bogo.springboot.domain.posts.PostsRepository;
 import com.bogo.springboot.web.dto.PostsSaveRequestDto;
 
+import com.bogo.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,5 +21,13 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id"+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
 
 }
--- src/main/java/com/bogo/springboot/web/PostsApiController.java
+++ src/main/java/com/bogo/springboot/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.bogo.springboot.web;
 import com.bogo.springboot.service.posts.PostsService;
 import com.bogo.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
