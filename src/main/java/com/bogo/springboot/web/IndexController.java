97be0e9ab810585199746ee6f8f4f18e41d3bc7b
acebogo <koprodev@gmail.com>
2020-09-28 01:31:21 +0900

    삭제 기능 추가

--- src/main/java/com/bogo/springboot/web/IndexController.java
+++ src/main/java/com/bogo/springboot/web/IndexController.java
@@ -5,6 +5,7 @@ import com.bogo.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
