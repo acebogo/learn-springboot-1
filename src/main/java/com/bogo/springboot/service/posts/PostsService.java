ddfeca2df42c2c2f83921f77fceb93018aad0bdb
acebogo <koprodev@gmail.com>
2020-09-27 22:33:47 +0900

    Save 기능을 위한 PostsApiController, PostsSaveRequestDto, PostsService 작업

---
+++ src/main/java/com/bogo/springboot/service/posts/PostsService.java
@@ -0,0 +1,23 @@
+package com.bogo.springboot.service.posts;
+
+import com.bogo.springboot.domain.posts.PostsRepository;
+import com.bogo.springboot.web.dto.PostsSaveRequestDto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+}
