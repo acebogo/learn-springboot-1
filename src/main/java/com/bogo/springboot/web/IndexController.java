39f7c280530f707808bd85731ed40ea3191a65f7
acebogo <koprodev@gmail.com>
2020-09-28 18:01:13 +0900

    @LoginUser 어노테이션을 이용한 SessionUser 정보 가져오기

--- src/main/java/com/bogo/springboot/web/IndexController.java
+++ src/main/java/com/bogo/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.bogo.springboot.web;
 
+import com.bogo.springboot.config.auth.LoginUser;
 import com.bogo.springboot.config.auth.dto.SessionUser;
 import com.bogo.springboot.service.posts.PostsService;
 import com.bogo.springboot.web.dto.PostsResponseDto;
@@ -18,13 +19,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
